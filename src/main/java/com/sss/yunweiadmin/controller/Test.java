ab26fcbd5c32e89b28d393328189aa42dad5d900
jonny201251 <jonny201251@163.com>
2022-04-16 22:01:26 +0800

    张强提交

--- src/main/java/com/sss/yunweiadmin/controller/Test.java
+++ src/main/java/com/sss/yunweiadmin/controller/Test.java
@@ -3,6 +3,6 @@ package com.sss.yunweiadmin.controller;
 public class Test {
     public static void main(String[] args) {
         System.out.println("test12");
-        System.out.println("aaaa");
+        System.out.println("aaaa111");
     }
 }
