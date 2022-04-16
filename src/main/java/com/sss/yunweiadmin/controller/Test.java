e38a25b4884432a479990fc3d34d156a3e25ddd3
jonny201251 <jonny201251@163.com>
2022-04-16 22:02:07 +0800

    张强提交

--- src/main/java/com/sss/yunweiadmin/controller/Test.java
+++ src/main/java/com/sss/yunweiadmin/controller/Test.java
@@ -2,6 +2,7 @@ package com.sss.yunweiadmin.controller;
 
 public class Test {
     public static void main(String[] args) {
+        System.out.println("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");
         System.out.println("test12");
         System.out.println("aaaa111");
     }
