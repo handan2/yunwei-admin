63a602a5f645c32a46748f13047a3e20d11e9df5
handan2 <448161065@qq.com>
2022-04-18 21:28:04 +0800

    aa1111

--- src/main/java/com/sss/yunweiadmin/controller/test2.java
+++ src/main/java/com/sss/yunweiadmin/controller/test2.java
@@ -4,6 +4,6 @@ import javax.xml.bind.SchemaOutputResolver;
 
 public class test2 {
     public static void main(String[] args) {
-        System.out.println("aaa11a23332177711");
+        System.out.println("aaa11a23332111000");
     }
 }
