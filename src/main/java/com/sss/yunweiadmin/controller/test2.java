8d205e810b52b6426358b16dc01adb4501b0b37f
handan2 <448161065@qq.com>
2022-04-18 20:41:27 +0800

    aa1111

--- src/main/java/com/sss/yunweiadmin/controller/test2.java
+++ src/main/java/com/sss/yunweiadmin/controller/test2.java
@@ -1,4 +1,9 @@
 package com.sss.yunweiadmin.controller;
 
+import javax.xml.bind.SchemaOutputResolver;
+
 public class test2 {
+    public static void main(String[] args) {
+        System.out.println("aaaa");
+    }
 }
