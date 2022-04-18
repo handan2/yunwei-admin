b267cde90cb3713aa794280e63290c733ee1af05
handan2 <448161065@qq.com>
2022-04-18 21:05:18 +0800

    aa1111

--- src/main/java/com/sss/yunweiadmin/controller/test2.java
+++ src/main/java/com/sss/yunweiadmin/controller/test2.java
@@ -4,6 +4,6 @@ import javax.xml.bind.SchemaOutputResolver;
 
 public class test2 {
     public static void main(String[] args) {
-        System.out.println("aaa11a22111");
+        System.out.println("aaa11a23332111");
     }
 }
